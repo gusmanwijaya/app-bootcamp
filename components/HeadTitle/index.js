be8a4b5a136f71c64de3eed64110acd06527186a
Gusman Wijaya <gusman.w.jaya@gmail.com>
2022-03-16 21:44:25 +0700

    fix bugs

--- components/HeadTitle/index.js
+++ components/HeadTitle/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default function HeadTitle({ title }) {
   return (
     <Head>
-      <meta charset="UTF-8" />
+      <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Semina | {title}</title>
